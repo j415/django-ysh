e561d1dd15a2b5e7eebae4cec8b487b6a1465f9b
j415 <1720049083@qq.com>
2018-07-27 09:40:27 +0800

    change a file

--- mysite/user/models.py
+++ mysite/user/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     nickname = models.CharField(max_length=20)
 
     def __str__(self):
-        return '<Profile:%s for %s>' % (self.nickname, self.user.username)
+        return '<Profile:%s for %s>' % (self.user.username, self.nickname)
 
 def get_nickname(self):
     if Profile.objects.filter(user=self).exists():

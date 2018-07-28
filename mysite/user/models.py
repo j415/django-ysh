ef70b28d11fb80089ab71a8a67f50ff344c127a4
j415 <1720049083@qq.com>
2018-07-28 22:04:14 +0800

    change user.models.Profile

--- mysite/user/models.py
+++ mysite/user/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='昵称')
-    nickname = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=20, verbose_name='昵称')
 
     def __str__(self):
         return '<Profile:%s for %s>' % (self.user.username, self.nickname)
